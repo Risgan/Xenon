aabc1bedec789249a9b1b6991943396e8039cefc
John Rueda <risgan2727@gmail.com>
2023-03-03 23:44:28 -0500

    Segundo commit

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { BalanceComponent } from './Pages/balance/balance.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { MovimientosComponent } from './Pages/movimientos/movimientos.component';
+import { PerfilComponent } from './Pages/perfil/perfil.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children:[
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        // pathMatch: 'full'
+      },
+      {
+        path: 'Movimientos',
+        component: MovimientosComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'Balance',
+        component: BalanceComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'Perfil',
+        component: PerfilComponent,
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
